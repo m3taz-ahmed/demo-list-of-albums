28a4b29df370fbbf808e9b112a9b915470698305
m3taz-ahmed <m3taz.ahmed@gmail.com>
2019-07-27 00:12:20 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,64 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import AppNavigator from '../AppNavigator';
 
-import React, { Component } from 'react';
-import { View, StyleSheet ,ActivityIndicator } from 'react-native';
 
-import { MyHeader } from './components/MyHeader';
-import { MyAlbum } from './components/MyAlbum';
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isLoading: true }
-  }
-
-  componentDidMount() {
-    return fetch('https://rallycoding.herokuapp.com/api/music_albums')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({
-          isLoading: false,
-          arrayOfData: responseJson,
-        }, function () {
-
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
+export default class App extends React.Component {
 
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{ flex: 1, padding: 50 }}>
-          <ActivityIndicator />
-        </View>
-      )
-    }
-
     return (
-      <View>
-        <MyHeader title='Albums!' textstyle={styles.header} />
-        <MyAlbum arrayOfData={this.state.arrayOfData} />
-      </View>
+      <AppNavigator />
     );
   }
 }
-
-
-
-const styles = StyleSheet.create({
-  header: {
-    height: 50,
-    fontSize: 25,
-    fontWeight: '600',
-    textAlign: 'center',
-    backgroundColor: '#fff',
-  },
-});
--- src/components/MyAlbum.js
+++ src/components/MyAlbum.js
@@ -1,42 +1,72 @@
-import React from 'react';
-import { Text, View, FlatList, Image, StyleSheet, Dimensions, TouchableOpacity, Linking } from 'react-native';
+import React , {Component} from 'react';
+import { Text, View, Image, StyleSheet, Dimensions, TouchableOpacity, Linking } from 'react-native';
 
-export const MyAlbum = ({ arrayOfData }) => {
 
-  _keyExtractor = (item, index) => item.title;
 
-  return (
-    <FlatList
-      data={arrayOfData}
-      renderItem={({ item }) =>
-        <View style={styles.album}>
-          <View style={styles.albumDetails}>
-            <View style={styles.thumbnailsContainer}>
-              <Image source={{ uri: item.thumbnail_image }} style={styles.thumbnails} />
-            </View>
-            <View style={styles.albumHeader}>
-              <Text style={styles.title}>{item.title}</Text>
-              <Text style={styles.artist}>{item.artist}</Text>
-            </View>
+export default class MyAlbum extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    // console.log('MyAlbum ', styles);
+    return (
+      <View style={{ flex:1,}}>
+      <View style={styles.container}>
+        <View style={styles.uppercontainer}>
+          <View style={styles.thumbnailscontainer}>
+            <Image style={styles.thumbnails} source={{ uri: this.props.navigation.getParam('thumbnail_image') }}  />
           </View>
-          <View style={styles.image}>
-            <Image source={{ uri: item.image }} style={styles.myImage} />
+          <View style={styles.albumHeader}>
+            <Text style={styles.title}>{this.props.navigation.getParam('title')}</Text>
+            <Text style={styles.artist}>{this.props.navigation.getParam('artist')}</Text>
           </View>
-          <TouchableOpacity style={styles.button} onPress={() => Linking.openURL(item.url)}>
+        </View>
+        <View style={styles.lowercontainer}>
+          <Image  style={styles.image} source={{ uri: this.props.navigation.getParam('image') }} />
+          <TouchableOpacity style={styles.button} onPress={() => Linking.openURL(this.props.navigation.getParam('url'))}>
             <View>
               <Text style={styles.buyNow}>Buy Now</Text>
             </View>
           </TouchableOpacity>
         </View>
-      }
-      keyExtractor={this._keyExtractor}
-    />
-  );
-};
+      </View>
+      </View>
+    );
+  };
+
+}
 
 const styles = StyleSheet.create({
-  album: {
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#333',
+    justifyContent: 'flex-start'
+  },
+  uppercontainer : {
+    flex: 2,
+    alignItems: 'stretch',
+    flexDirection: 'column',
     margin: 10,
+    justifyContent: 'flex-start',
+  },
+  lowercontainer: {
+    flex: 4,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+
+  },
+  thumbnailscontainer: {
+
+  },
+  thumbnails: {
+    width: 45,
+    height: 45
+  },
+  albumHeader: {
+
   },
   title: {
     fontSize: 16,
@@ -46,8 +76,13 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: '600',
   },
+  image: {
+    width: Dimensions.get('window').width * 0.94,
+    height : Dimensions.get('window').height * 0.5,
+  },
   button: {
     flex: 1,
+    alignItems: 'stretch',
     padding: 15,
     fontSize: 16,
     fontWeight: '600',
@@ -58,39 +93,67 @@ const styles = StyleSheet.create({
     textAlignVertical: 'center',
     textAlign: 'center',
   },
-  image: {
-    flex: 1,
-    flexDirection: 'row',
-    borderColor: '#dedede',
-    borderWidth: 1,
-    alignContent: 'center',
-  },
-  albumDetails: {
-    flex: 2,
-    padding: 3,
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    borderColor: '#dedede',
-    borderWidth: 1
-  },
-  thumbnailsContainer: {
-    width: 50, height: 50
-  },
-  thumbnails: {
-    width: 45,
-    height: 45
-  },
-  albumHeader: {
-    paddingLeft: 10,
-    width: 200,
-    height: 50
-  },
-  myImage: {
-    width: Dimensions.get('window').width * 0.94,
-    height: 300
-  },
   buyNow: {
-    textAlignVertical: 'center',
-    textAlign: 'center'
+
   }
+  // album: {
+  //   flex: 1,
+  //   margin: 10,
+  // },
+  // title: {
+  //   fontSize: 16,
+  //   fontWeight: '600',
+  // },
+  // artist: {
+  //   fontSize: 14,
+  //   fontWeight: '600',
+  // },
+  // button: {
+  //   flex: 1,
+  //   padding: 15,
+  //   fontSize: 16,
+  //   fontWeight: '600',
+  //   color: '#23eae6',
+  //   borderRadius: 4,
+  //   borderWidth: 1,
+  //   borderColor: '#23eae6',
+  //   textAlignVertical: 'center',
+  //   textAlign: 'center',
+  // },
+  // image: {
+  //   flex: 3,
+  //   borderColor: '#dedede',
+  //   borderWidth: 1,
+  //   alignContent: 'center',
+  // },
+  // albumDetails: {
+  //   flex: 1,
+  //   padding: 3,
+  //   flexDirection: 'row',
+  //   alignItems: 'flex-start',
+  //   borderColor: '#dedede',
+  //   borderWidth: 1
+  // },
+  // thumbnailsContainer: {
+  //   flex: 2,
+  //   width: 50, height: 50
+  // },
+  // thumbnails: {
+  //   width: 45,
+  //   height: 45
+  // },
+  // albumHeader: {
+  //   paddingLeft: 10,
+  //   width: 200,
+  //   height: 50,
+  //   flex: 4,
+  // },
+  // myImage: {
+  //   width: Dimensions.get('window').width * 0.94,
+  //   height: 300
+  // },
+  // buyNow: {
+  //   textAlignVertical: 'center',
+  //   textAlign: 'center'
+  // }
 });
